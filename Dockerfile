6422cb85a23060188826a754ff4523e665c7db54
Fernando Barbosa <fbcbarbosa@gmail.com>
2018-06-21 10:22:41 -0300

    Add devops project

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-# This example python Dockerfile is incomplete!
-#FROM python:2
-#RUN pip install ...
-#ADD . .
-#CMD do something
-#EXPOSE 80
---
+++ devops/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+import requests
+
+
+@app.route('/', methods=["GET", "POST"])
+def hello():
+    payload = request.get_json()
+    print payload
+    payload = {
+        "company": "QuintoAndar",
+        "contactOptIn": True,
+        "email": "dqueiroz@quintoandar.com.br",
+        "name": "Didi do QuintoAndar",
+        "phone": "+5511947073916"
+    }
+    return jsonify(
+        requests.post(
+            "https://aws-challenge.quintoandar.com.br/contact/",
+            json=payload).json())
+
+
+if __name__ == '__main__':
+    app.run()
